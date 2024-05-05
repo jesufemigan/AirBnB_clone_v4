5af17dbc986bb21a8f512a46cf220cb2af959080
Jesufemi <oladapofemmy@gmail.com>
2024-05-05 16:16:10 +0100

    task 0-2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+function handleAmenities() {
+   let amenities = {};
+    $('input[type="checkbox"]').change(function() {
+        if ($(this).is(':checked')) {
+            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+        } else {
+            delete amenities[$(this).attr('data-id')];
+        }
+        $(".amenities h4").text(Object.values(amenities).join(', '));
+    });
+}
+$( document ).ready(handleAmenities);
